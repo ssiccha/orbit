8e2605a3535bb408ff244858c629d87efed3e448
Sergio Siccha <sergio.siccha@gmail.com>
2017-01-31 19:13:22 +0100

    Add debug mode and pipe usage to scripts

--- scripts/runarchsym.sh
+++ scripts/runarchsym.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 # runarchsym.sh
 
-if [ "$#" != "3" ]; then
-   echo "Usage: runarchsym.sh <app-name> <arch-name> <input-filename>"
-   exit 1
+if [ "$#" != "3" -a "$#" != "4" ]; then
+    echo "Usage: runarchsym.sh <app-name> <arch-name> <in-pipe-filename> <debug>"
+    echo -en "\nAny 4th argument will trigger debug mode.\n"
+    exit 1
 fi;
 
-cd ../gap
+SCRIPTSDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASEDIR=${SCRIPTSDIR%'/scripts'} # remove script directory
+GAPDIR=${BASEDIR}"/gap"
+cd ${GAPDIR}
 
-## DEBUG ##
-#echo "gap read.g"
-#echo "MappingsCacheLookup( \"${1}\", \"${2}\", \"${3}\", \"dummyOutStream\" );"
-#exit 1
-## DEBUG ##
+## DEBUG MODE ##
+if [ "$#" == "4" ]; then
+    echo "Run the following commands to debug:"
+    echo "gap read.g"
+    echo "MappingsCacheLookup( \"${1}\", \"${2}\", \"${3}\", \"dummyOutStream\" );"
+    echo "rm ${3}"
+    exit 1
+fi;
 
+## NORMAL RUN ##
 gap -o 50g -L ~/.gap/emptyWorkspace -r -b -q read.g << EOI
 tmp := GET_REAL_TIME_OF_FUNCTION_CALL(
     MappingsCacheLookup,
     [ "$1", "$2", "$3", "dummyOutStream" ],
     rec( passResult := true )
 );;
-res := tmp.result;;
-t := tmp.time;;
-res.sizeOmega;
-res.numberOrbits;
-t;
+if IsBound( tmp ) then
+    res := tmp.result;;
+    t := tmp.time;;
+    res.sizeOmega;
+    res.numberOrbits;
+    t;
+fi;
 EOI
 
--- scripts/test-suite.sh
+++ scripts/test-suite.sh
@@ -1,52 +1,72 @@
 #!/bin/bash
 # test-suite.sh
 
-if [ "$#" != "2" ]; then
-    echo -en "Usage: \ttest-suite.sh <app-name> <arch-name>\n"
+if [ "$#" != "2" -a "$#" != "3" ]; then
+    echo -en "Usage: \ttest-suite.sh <app-name> <arch-name> <debug>\n"
     echo -en "app-name: \taudio_filter_3, jpeg, "
     echo -en "mjpeg_compaan, sobel, mandelbrot\n"
-    echo -en "arch-name: \ts4, s4xs8\n"
+    echo -en "arch-name: \ts4, s4xs8\n\n"
+    echo -en "Any 3rd argument will trigger debug mode.\n"
     exit 1
+fi;
+
+if [ "$#" == "3" ]; then
+    echo "Debug run."
 fi;
 
 APP="$1"
 ARCH="$2"
-PWD=`pwd`
-BASEDIR=${PWD%'/scripts'} # remove script directory
+SCRIPTSDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASEDIR=${SCRIPTSDIR%'/scripts'} # remove script directory
 DATADIR=${BASEDIR}"/data"
 OUTPUTDIR=${BASEDIR}"/results"
+OUTPUTDIR=${OUTPUTDIR}/${APP}--`date +%F--%H-%M`
 GAPDIR=${BASEDIR}"/gap"
 GAPSCRIPT=${BASEDIR}"/scripts/runarchsym.sh"
 
-DATAFILES=""
+DATAFILENAMES=""
 for file in `ls $DATADIR/${APP}.$ARCH*`; do
-    DATAFILES="${DATAFILES} ${file#$DATADIR/}"
+    DATAFILENAMES="${DATAFILENAMES} ${file#$DATADIR/}"
 done
 echo "Running experiments for architecture ${ARCH} and"
-echo ${DATAFILES}
+echo ${DATAFILENAMES}
+echo ""
 
-#OUTPUTDIR=$OUTPUTDIR/results-`date +%F-%H-%M`
-OUTPUTDIR=$OUTPUTDIR/${APP}--`date +%F--%H-%M`
-mkdir -p $OUTPUTDIR
+## DEBUG RUN ##
+if [ "$#" == "3" ]; then
+    for filename in $DATAFILENAMES; do
+        pipe="../pipes/"${filename}"-in-pipe"
+        mkfifo ${pipe}
+        cat ${DATADIR}/${filename} >> ${pipe} && echo "" >> ${pipe} &
+        $GAPSCRIPT $APP $ARCH ${pipe} 1
+        exit 1
+    done
+fi;
 
 cd $GAPDIR/
 
-for file in $DATAFILES; do
-    outfile="$OUTPUTDIR/results${file#${APP}.${ARCH}}"
-    ## DEBUG ##
-    #$GAPSCRIPT $APP $ARCH ${file#$DATADIR/}
-    #exit 1
-    ## DEBUG ##
-    $GAPSCRIPT $APP $ARCH ${file} 2>$OUTPUTDIR/errors.txt > $outfile&
+mkdir -p $OUTPUTDIR
+mkdir -p $BASEDIR"/pipes"
+
+## NORMAL RUN ##
+## TODO make it run in parallel!!
+for filename in $DATAFILENAMES; do
+    echo ${filename}
+    pipe="../pipes/"${filename}"-in-pipe"
+    mkfifo ${pipe}
+    cat ${DATADIR}/${filename} >> ${pipe} && echo "" >> ${pipe} &
+    outfile="$OUTPUTDIR/results${filename#${APP}.${ARCH}}"
+    $GAPSCRIPT $APP $ARCH ${pipe} 2>$OUTPUTDIR/errors.txt > $outfile
+    rm ${pipe}
 done
 
 wait
 
-for file in $DATAFILES; do
-    outfile="$OUTPUTDIR/results${file#${APP}.${ARCH}}"
+for filename in $DATAFILENAMES; do
+    outfile="$OUTPUTDIR/results${filename#${APP}.${ARCH}}"
     allresults="$OUTPUTDIR/allresults.${APP}.${ARCH}.txt"
     echo "=========================" >> $allresults
-    echo $file >> $allresults
+    echo $filename >> $allresults
     echo "-------------------------" >> $allresults
     tail -n 3 $outfile >> $allresults
 done
