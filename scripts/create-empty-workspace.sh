fef5d68f71d865cd3ce07efa3c9def16266bd098
Sergio Siccha <sergio.siccha@gmail.com>
2017-02-01 16:18:10 +0100

    Fix tabs + spaces

--- scripts/create-empty-workspace.sh
+++ scripts/create-empty-workspace.sh
@@ -4,19 +4,18 @@ WORKSPACE_FILE="~/.gap/emptyWorkspace"
 
 #does the file exist already?
 if [ ! -d ~/.gap/ ]; then 
-	 mkdir ~/.gap
+    mkdir ~/.gap
 fi
 
 if [ -f $WORKSPACE_FILE ]; then
     exit 0
 else
-	 if [ -e $WORKSPACE_FILE ]; then
-		  >&2 "Error. file $WORKSPACE_FILE exists but is not a regular file." 
-		  exit 1
-	 fi 
+    if [ -e $WORKSPACE_FILE ]; then
+        >&2 "Error. file $WORKSPACE_FILE exists but is not a regular file." 
+        exit 1
+    fi 
 fi 
 
 #file does not exist. create it!
-
 $GAP_BIN -b -q  << EOI
 SaveWorkspace("$WORKSPACE_FILE");
